2d11bb65eb64eea727dceb5c8a465f5434306386
Katarina Marinković <marinkovic.pr134.2020@uns.ac.rs>
2023-06-06 17:10:51 +0200

    table view / filter add

--- NetworkService/ViewModel/EntitiesViewModel.cs
+++ NetworkService/ViewModel/EntitiesViewModel.cs
@@ -10,6 +10,14 @@ namespace NetworkService.ViewModel
 {
     public class EntitiesViewModel : BindableBase
     {
+        private enum CLASSES { ONE, TWO, THREE };
+        private int chosenClassesAddresses;
         private static readonly ObservableCollection<string> Addresses = new ObservableCollection<string> { "Address scope 1", "Address scope 2", "Address scope 3"};
+
+        private int ChosenID;
+
+        private bool LessIsChecked;
+        private bool MoreIsChecked;
+        private bool EqualsIsChecked;
     }
 }
