e10fd1c5b327ef4675f9dd783c9049ee09803512
Katarina Marinković <marinkovic.pr134.2020@uns.ac.rs>
2023-06-06 23:52:24 +0200

    message class

---
+++ NetworkService/Model/MessageChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace NetworkService.Model
+{
+    public class MessageChange
+    {
+        public Visibility Visibility_Success { get; set; }
+        public Visibility Visibility_Error { get; set; }
+        public string Mess { get; set; }
+
+        public MessageChange()
+        {
+
+        }
+    }
+}
