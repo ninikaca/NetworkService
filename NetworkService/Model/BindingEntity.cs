a7bf4cbdd8136b928201c8ad9d6402416862f7ea
Katarina Marinković <marinkovic.pr134.2020@uns.ac.rs>
2023-06-07 19:45:48 +0200

    add binding class

---
+++ NetworkService/Model/BindingEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace NetworkService.Model
+{
+    class BindingEntity
+    {
+        public string addresses { get; set; }
+        public BindingList<Entity> listOfEntities { get; set; }
+
+        public BindingEntity()
+        {
+            listOfEntities = new BindingList<Entity>();
+        }
+    }
+}
--- NetworkService/ViewModel/ViewViewModel.cs
+++ NetworkService/ViewModel/ViewViewModel.cs
@@ -1,13 +1,23 @@
-﻿using NetworkService.Helpers;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using MVVMLight.Messaging;
 using NetworkService.Model;
-using System;
-using System.Collections.ObjectModel;
-using System.lO;
+using NetworkService.Views;
+using System.Windows.Media;
+using System.ComponentModel;
+using NetworkService.Helpers;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
 
 namespace NetworkService.ViewModel
 {
     public class ViewViewModel : BindableBase
     {
-        
+        //binding elementi
+        public static BindingList<KlasifikovaniEntiteti> EntitetiCanvas { get; set; }
+        public static BindingList<KlasifikovaniEntiteti> EntitetiTreeView { get; set; }
     }
 }
